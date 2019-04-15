fb212e668ea873ac34a240958f89689ef421b6e2
Alex Borst <butler.borst@gmail.com>
2019-04-15 17:20:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,8 +4,8 @@ def using_push(array, string)
     array.push(string)
 end
 
-def using_shift(array, string)
+def using_unshift(array, string)
     array = []
     string = "Brooklyn Heights"
-    array.push(string)
+    array.unshift(string)
 end
